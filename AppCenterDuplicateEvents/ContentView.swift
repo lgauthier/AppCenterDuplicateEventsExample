d74f04a3e1dce36c76348554b00c5fd6642336d7
Logan Gauthier <logan.paul.gauthier@gmail.com>
2019-10-02 10:08:44 -0500

    Initial Commit

---
+++ AppCenterDuplicateEvents/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  AppCenterDuplicateEvents
+//
+//  Created by Logan Gauthier on 10/2/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
